dc58ecc7660692de68799ff5b37f2a96b858fd8d
persianpros <persianpros@yahoo.com>
2020-04-09 03:11:38 +0430

    Follow correct folder structure for distutils.

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ import setup_translate
 pkg = 'Extensions.OpenVisionSkinTools'
 setup (name = 'enigma2-plugin-extensions-openvisionskintools',
        version = '1.0',
-       description = 'Plugin to skin style.',
+       description = 'Tool for changing skin styles',
        packages = [pkg],
-       package_dir = {pkg: 'usr'},
+       package_dir = {pkg: 'plugin'},
        package_data = {pkg: ['plugin.png', '*/*.png', 'locale/*/LC_MESSAGES/*.mo']},
        cmdclass = setup_translate.cmdclass, # for translation
       )
